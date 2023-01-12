18349e878739a6a1f0146956f71918f1cd2af3cb
Alessandro Dodi <dodialessandro3@gmail.com>
2023-01-12 23:12:46 +0100

    Filters

--- lib/screens/filters_sreen.dart
+++ lib/screens/filters_sreen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
   const FiltersScreen({super.key});
 
   @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegeterial = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('filters'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text('Adjust your meal selection',
+                style: Theme.of(context).textTheme.titleLarge),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              SwitchListTile(
+                title: Text('Gluten free'),
+                value: _glutenFree,
+                subtitle: Text('Only includes gluten free meals'),
+                onChanged: (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                },
+              )
+            ],
+          ))
+        ],
       ),
     );
   }
